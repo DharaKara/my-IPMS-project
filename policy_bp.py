7b178940122a46cc91bc75fe7bba5c5201c6882c
dharakara <dhara.kara@sanlam.co.za>
2024-04-02 22:29:42 +0200

    view more details on products

--- policy_bp.py
+++ policy_bp.py
@@ -8,3 +8,9 @@ policy_bp = Blueprint("policy_bp", __name__)
 def policy():
     policy = Policy.query.all()
     return render_template("policies.html", policy=policy)
+
+
+@policy_bp.route("/policies/<id>")
+def product_details(id):
+    policy = Policy.query.get_or_404(id)
+    return render_template("policies-details.html", policy=policy)
